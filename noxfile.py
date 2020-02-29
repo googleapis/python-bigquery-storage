c27e4ca985948bca668226b4c48e19ccb13bed68
Seth Hollyman <shollyman@google.com>
2020-02-29 01:01:56 +0000

    feat: add manual layer for v1 endpoint
    * adds a per-endpoint system session in nox
    * migrate the manual client/reader work for v1
    * TableReferences went away as a first class message, now just a
      formatted string
    * changes to read rows
      * estimated_row_count removed due to differences in reported status
      * you no longer have to deal with a StreamPosition message, instead
        there's just a stream name and an offset as top-level request fields
    * session creation changes
      * you now provide a prototypical ReadSession message when requesting
        a new read session, and most options (like selected fields, table
        modifiers, and data format) have moved inside it.
      * requested_streams -> max_stream_count
    
    There's additional changes to the surface, but there wasn't much manual
    help in front of it.

--- noxfile.py
+++ noxfile.py
@@ -93,9 +93,50 @@ def unit(session):
 
 @nox.session(python=["2.7", "3.7"])
 def system(session):
-    """Run the system test suite."""
-    system_test_path = os.path.join("tests", "system.py")
-    system_test_folder_path = os.path.join("tests", "system")
+    """Run all the endpoint system tests."""
+    system_v1beta1(session)
+    system_v1(session)
+
+
+@nox.session(python=["2.7", "3.7"])
+def system_v1beta1(session):
+    """Run the v1beta1 endpoint system test suite."""
+    system_test_path = os.path.join("tests/v1beta", "system.py")
+    system_test_folder_path = os.path.join("tests", "system/v1beta1")
+    # Sanity check: Only run tests if the environment variable is set.
+    if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", ""):
+        session.skip("Credentials must be set via environment variable")
+
+    system_test_exists = os.path.exists(system_test_path)
+    system_test_folder_exists = os.path.exists(system_test_folder_path)
+    # Sanity check: only run tests if found.
+    if not system_test_exists and not system_test_folder_exists:
+        session.skip("System tests were not found")
+
+    # Use pre-release gRPC for system tests.
+    session.install("--pre", "grpcio")
+
+    session.install("google-cloud-bigquery")
+
+    # Install all test dependencies, then install this package into the
+    # virtualenv's dist-packages.
+    session.install("mock", "pytest")
+
+    session.install("-e", ".[fastavro,pandas,pyarrow]")
+    session.install("-e", ".")
+
+    # Run py.test against the system tests.
+    if system_test_exists:
+        session.run("py.test", "--quiet", system_test_path, *session.posargs)
+    if system_test_folder_exists:
+        session.run("py.test", "--quiet", system_test_folder_path, *session.posargs)
+
+
+@nox.session(python=["2.7", "3.7"])
+def system_v1(session):
+    """Run the v1 endpoint system test suite."""
+    system_test_path = os.path.join("tests/v1beta", "system.py")
+    system_test_folder_path = os.path.join("tests", "system/v1")
     # Sanity check: Only run tests if the environment variable is set.
     if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", ""):
         session.skip("Credentials must be set via environment variable")
