f5255226967b03cfee55901600d864267cd93952
yoshi-automation <yoshi-automation@google.com>
2021-05-15 05:10:57 +0000

    fix: add async client to %name_%version/init.py chore: add autogenerated snippets chore: remove auth, policy, and options from the reserved names list feat: support self-signed JWT flow for service accounts chore: enable GAPIC metadata generation chore: sort subpackages in %namespace/%name/init.py
    PiperOrigin-RevId: 372197450
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed May 5 13:39:02 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 83a7e1c8c2f7421ded45ed323eb1fda99ef5ea46
    Source-Link: https://github.com/googleapis/googleapis/commit/83a7e1c8c2f7421ded45ed323eb1fda99ef5ea46

--- google/cloud/bigquery_storage_v1/services/big_query_read/client.py
+++ google/cloud/bigquery_storage_v1/services/big_query_read/client.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 from collections import OrderedDict
 from distutils import util
 import os
@@ -23,10 +21,10 @@ from typing import Callable, Dict, Optional, Iterable, Sequence, Tuple, Type, Un
 import pkg_resources
 
 from google.api_core import client_options as client_options_lib  # type: ignore
-from google.api_core import exceptions  # type: ignore
+from google.api_core import exceptions as core_exceptions  # type: ignore
 from google.api_core import gapic_v1  # type: ignore
 from google.api_core import retry as retries  # type: ignore
-from google.auth import credentials  # type: ignore
+from google.auth import credentials as ga_credentials  # type: ignore
 from google.auth.transport import mtls  # type: ignore
 from google.auth.transport.grpc import SslCredentials  # type: ignore
 from google.auth.exceptions import MutualTLSChannelError  # type: ignore
@@ -36,8 +34,7 @@ from google.cloud.bigquery_storage_v1.types import arrow
 from google.cloud.bigquery_storage_v1.types import avro
 from google.cloud.bigquery_storage_v1.types import storage
 from google.cloud.bigquery_storage_v1.types import stream
-from google.protobuf import timestamp_pb2 as timestamp  # type: ignore
-
+from google.protobuf import timestamp_pb2  # type: ignore
 from .transports.base import BigQueryReadTransport, DEFAULT_CLIENT_INFO
 from .transports.grpc import BigQueryReadGrpcTransport
 from .transports.grpc_asyncio import BigQueryReadGrpcAsyncIOTransport
@@ -270,7 +267,7 @@ class BigQueryReadClient(metaclass=BigQueryReadClientMeta):
     def __init__(
         self,
         *,
-        credentials: Optional[credentials.Credentials] = None,
+        credentials: Optional[ga_credentials.Credentials] = None,
         transport: Union[str, BigQueryReadTransport, None] = None,
         client_options: Optional[client_options_lib.ClientOptions] = None,
         client_info: gapic_v1.client_info.ClientInfo = DEFAULT_CLIENT_INFO,
@@ -450,7 +447,6 @@ class BigQueryReadClient(metaclass=BigQueryReadClientMeta):
                 This corresponds to the ``max_stream_count`` field
                 on the ``request`` instance; if ``request`` is provided, this
                 should not be set.
-
             retry (google.api_core.retry.Retry): Designation of what errors, if any,
                 should be retried.
             timeout (float): The timeout for this request.
@@ -477,10 +473,8 @@ class BigQueryReadClient(metaclass=BigQueryReadClientMeta):
         # there are no flattened fields.
         if not isinstance(request, storage.CreateReadSessionRequest):
             request = storage.CreateReadSessionRequest(request)
-
             # If we have keyword arguments corresponding to fields on the
             # request, apply these.
-
             if parent is not None:
                 request.parent = parent
             if read_session is not None:
@@ -543,7 +537,6 @@ class BigQueryReadClient(metaclass=BigQueryReadClientMeta):
                 This corresponds to the ``offset`` field
                 on the ``request`` instance; if ``request`` is provided, this
                 should not be set.
-
             retry (google.api_core.retry.Retry): Designation of what errors, if any,
                 should be retried.
             timeout (float): The timeout for this request.
@@ -572,10 +565,8 @@ class BigQueryReadClient(metaclass=BigQueryReadClientMeta):
         # there are no flattened fields.
         if not isinstance(request, storage.ReadRowsRequest):
             request = storage.ReadRowsRequest(request)
-
             # If we have keyword arguments corresponding to fields on the
             # request, apply these.
-
             if read_stream is not None:
                 request.read_stream = read_stream
             if offset is not None:
@@ -625,7 +616,6 @@ class BigQueryReadClient(metaclass=BigQueryReadClientMeta):
             request (google.cloud.bigquery_storage_v1.types.SplitReadStreamRequest):
                 The request object. Request message for
                 `SplitReadStream`.
-
             retry (google.api_core.retry.Retry): Designation of what errors, if any,
                 should be retried.
             timeout (float): The timeout for this request.
@@ -637,7 +627,6 @@ class BigQueryReadClient(metaclass=BigQueryReadClientMeta):
                 Response message for SplitReadStream.
         """
         # Create or coerce a protobuf request object.
-
         # Minor optimization to avoid making a copy if the user passes
         # in a storage.SplitReadStreamRequest.
         # There's no risk of modifying the input as we've already verified
