4a6345eaeb2512c94e067dae394c82bdb3528a83
Tim Swast <swast@google.com>
2021-09-17 15:59:41 -0500

    WIP: benchmark generates data

--- benchmark/appendrows/append_rows_benchmark/__main__.py
+++ benchmark/appendrows/append_rows_benchmark/__main__.py
@@ -13,18 +13,98 @@
 # limitations under the License.
 
 import argparse
+import collections
+import pathlib
+import time
+
+from google.cloud import bigquery
+from google.cloud import bigquery_storage_v1beta2
+from google.cloud.bigquery_storage_v1beta2 import types
+from google.cloud.bigquery_storage_v1beta2 import writer
+from google.protobuf import descriptor_pb2
+
+from append_rows_benchmark import data_pb2
+
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+
+
+def create_table(project_id, dataset_id, table_id):
+    bqclient = bigquery.Client()
+    schema = bqclient.schema_from_json(CURRENT_DIR / "data_bq.json")
+    table = bigquery.Table(f"{project_id}.{dataset_id}.{table_id}", schema=schema)
+    bqclient.create_table(table, exists_ok=True)
+
+
+StreamData = collections.namedtuple(
+    "StreamData", ["append_rows_stream", "stream_name", "write_client", "parent"]
+)
 
 
 def start_stream(project_id, dataset_id, table_id):
-    pass
+    write_client = bigquery_storage_v1beta2.BigQueryWriteClient()
+    parent = write_client.table_path(project_id, dataset_id, table_id)
+    write_stream = types.WriteStream()
+
+    # TODO: allow for default stream, buffered, and pending types.
+    write_stream.type_ = types.WriteStream.Type.COMMITTED
+    write_stream = write_client.create_write_stream(
+        parent=parent, write_stream=write_stream
+    )
+    stream_name = write_stream.name
+
+    # Create a template with fields needed for the first request.
+    request_template = types.AppendRowsRequest()
+
+    # The initial request must contain the stream name.
+    request_template.write_stream = stream_name
+
+    # So that BigQuery knows how to parse the serialized_rows, generate a
+    # protocol buffer representation of your message descriptor.
+    proto_schema = types.ProtoSchema()
+    proto_descriptor = descriptor_pb2.DescriptorProto()
+    data_pb2.StressData.DESCRIPTOR.CopyToProto(proto_descriptor)
+    proto_schema.proto_descriptor = proto_descriptor
+    proto_data = types.AppendRowsRequest.ProtoData()
+    proto_data.writer_schema = proto_schema
+    request_template.proto_rows = proto_data
+
+    # Some stream types support an unbounded number of requests. Construct an
+    # AppendRowsStream to send an arbitrary number of requests to a stream.
+    append_rows_stream = writer.AppendRowsStream(write_client, request_template)
+    return StreamData(append_rows_stream, stream_name, write_client, parent)
 
 
-def main(project_id, dataset_id, table_id):
-    pass
+def wait_then_close(time_to_wait, stream: StreamData):
+    start_time = time.monotonic()
+    end_time = start_time + time_to_wait
+    now = time.monotonic()
+    while now < end_time:
+        time.sleep(end_time - now)
+        now = time.monotonic()
+
+    stream.append_rows_stream.close()
+
+    # A PENDING type stream must be "finalized" before being committed.
+    # write_client.finalize_write_stream(name=write_stream.name)
+
+    # Commit the stream you created earlier.
+    batch_commit_write_streams_request = types.BatchCommitWriteStreamsRequest()
+    batch_commit_write_streams_request.parent = stream.parent
+    batch_commit_write_streams_request.write_streams = [stream.stream_name]
+    stream.write_client.batch_commit_write_streams(batch_commit_write_streams_request)
+
+
+def main(project_id, dataset_id, table_id, num_workers=8):
+    create_table(project_id, dataset_id, table_id)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers + 1):
+    stream = start_stream(project_id, dataset_id, table_id)
+    wait_then_close(60, stream)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("full_table_id")
+    parser.add_argument("--num-workers", type=int, default=8)
     args = parser.parse_args()
-    main(*args.full_table_id.split("."))
+    main(*args.full_table_id.split("."), num_workers=args.num_workers)
---
+++ benchmark/appendrows/append_rows_benchmark/worker.py
@@ -0,0 +1,77 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+
+from google.cloud.bigquery_storage_v1beta2 import exceptions as bqstorage_exceptions
+from google.cloud.bigquery_storage_v1beta2 import types
+
+from append_rows_benchmark import data_pb2
+
+
+EPOCH_VALUE = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
+
+
+def main(append_rows_stream):
+    while append_rows_stream.is_active:
+        request = types.AppendRowsRequest()
+        proto_data = types.AppendRowsRequest.ProtoData()
+        proto_rows = types.ProtoRows()
+        dense_row_generator(proto_rows)
+        proto_data.rows = proto_rows
+        request.proto_rows = proto_data
+        try:
+            append_rows_stream.send(request)
+        except bqstorage_exceptions.StreamClosedError:
+            # Done with writes.
+            return
+
+
+def dense_row_generator(proto_rows):
+    """Generates rows up to a predetermined size limit."""
+    available = 8 * 1024 * 1024
+    rows = []
+    while True:
+        row_bytes = b"hello world" * 20
+        row = data_pb2.StressData()
+        now = datetime.datetime.now(tz=datetime.timezone.utc)
+        row.bool_value = (now.minute % 2) == 0
+        row.bytes_value = b"so there i was"
+        row.date_value = int(now.timestamp()) // 86400
+        row.datetime_value = "2016-05-19T10:38:47.046465"
+        row.geography_value = "POINT(-122.350220 47.649154)"
+        row.int64_value = int(now.timestamp() * 1000)
+        row.numeric_value = now.strftime("%Y%m%d%H%M%S.%f")
+        row.bignumeric_value = now.strftime("%Y%m%d%H%M%S.%f")
+        row.float64_value = 1.23456
+        row.string_value = now.isoformat()
+        delta = now - EPOCH_VALUE
+        row.timestamp_value = int(delta.total_seconds()) * 1000000 + int(
+            delta.microseconds
+        )
+        row.single_struct.values.append(1)
+        row.single_struct.values.append(2)
+        row.single_struct.values.append(3)
+        row.single_struct.values.append(4)
+        row.single_struct.values.append(5)
+        row.pairs.append(data_pb2.StressData.KeyValuePair(key="blah", value="foo",))
+        row.pairs.append(data_pb2.StressData.KeyValuePair(key="blah2", value="foo",))
+        row_bytes = row.SerializeToString()
+
+        if available - len(row_bytes) < 0:
+            break
+        available = available - len(row_bytes)
+        rows.append(row_bytes)
+
+    proto_rows.serialized_rows = rows
