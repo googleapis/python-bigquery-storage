117861480943c37063e3c2854bbd2e1c185bf988
Bu Sun Kim <busunkim@google.com>
2022-03-10 23:22:45 +0000

    chore(deps): allow google-api-core v2 on v1 release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ description = "BigQuery Storage API API client library"
 version = "1.0.0"
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
+    "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
     'enum34; python_version < "3.4"',
 ]
 extras = {
