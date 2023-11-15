92fd70b77e3db98b64c3525bd4d73b39a8297a7a
kiraksi <kirnendra@google.com>
2023-11-15 00:54:02 -0800

    removed pkg_resources

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ dependencies = [
     "proto-plus >= 1.22.0, <2.0.0dev",
     "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
     "protobuf>=3.19.5,<5.0.0dev,!=3.20.0,!=3.20.1,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
+    "importlib_metadata<=4.13.0; python_version<'3.8'",
 ]
 extras = {
     "pandas": ["pandas>=0.21.1"],
@@ -58,9 +59,13 @@ with io.open(readme_filename, encoding="utf-8") as readme_file:
 
 packages = [
     package
-    for package in setuptools.find_namespace_packages()
+    for package in setuptools.PEP420PackageFinder.find()
     if package.startswith("google")
 ]
+
+namespaces = ["google"]
+if "google.cloud" in packages:
+    namespaces.append("google.cloud")
 
 setuptools.setup(
     name=name,
@@ -88,6 +93,7 @@ setuptools.setup(
     platforms="Posix; MacOS X; Windows",
     packages=packages,
     python_requires=">=3.7",
+    namespace_packages=namespaces,
     install_requires=dependencies,
     extras_require=extras,
     scripts=["scripts/fixup_bigquery_storage_v1_keywords.py"],
