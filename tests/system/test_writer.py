326c274be6623ce9a5876198b738d5208ea04b5c
A <5249513+Dumeng@users.noreply.github.com>
2024-01-26 11:36:12 +0800

    Fix typo in the test

--- tests/system/test_writer.py
+++ tests/system/test_writer.py
@@ -81,7 +81,7 @@ def test_append_rows_with_proto3(bqstorage_write_client, table):
 
     person_pb = PersonProto.pb()
 
-    stream_name = f"projects/{table.project_id}/datasets/{table.dataset_id}/tables/{table.table_id}/_default"
+    stream_name = f"projects/{table.project}/datasets/{table.dataset_id}/tables/{table.table_id}/_default"
     request_template = gapic_types.AppendRowsRequest()
     request_template.write_stream = stream_name
 
